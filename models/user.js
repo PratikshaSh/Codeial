3082b9e706c1d5026b8493d286ab809ea7aa9cca
PratikshaSh <pratiksha8520@gmail.com>
2021-02-10 16:54:05 +0530

    create schema for users signup form

---
+++ models/user.js
@@ -0,0 +1,24 @@
+//For creating user schema
+const mongoose = require('mongoose');
+
+//Schema for sign up form
+ const userSchema = new mongoose.Schema({
+     email:{
+         type: String,
+         required: true,
+         unique: true
+     },
+     password:{
+         type:String,
+         required:true
+     },
+     name:{
+         type: String,
+         required: true
+     }
+ },{
+     timestamps: true
+ });
+
+ const User = mongoose.model('User',userSchema);
+ module.exports=User;
