6fd40614728f4e17e5bea204f27d94d4b20243b3
PratikshaSh <pratiksha8520@gmail.com>
2021-02-10 21:22:44 +0530

    Add layout

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
 
 //use express router
 app.use('/', require('./routes'));
 
 //set up the view engine
-app.set('view engine','ejs');
-app.set('views','./views');
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
-app.listen(port, function(err){
-    if(err){
+app.listen(port, function(err) {
+    if (err) {
         //console.log('Error:',err);
-        console.log(`Error in running the server: ${err}`);//interpolation
+        console.log(`Error in running the server: ${err}`); //interpolation
     }
     console.log(`Server is running on port: ${port}`);
 
