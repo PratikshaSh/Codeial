45db910bb0eeddc84a73697a7378032abd5c7e85
PratikshaSh <pratiksha8520@gmail.com>
2021-02-10 20:59:57 +0530

    changed rendering file

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,22 @@
-module.exports.profile = function(req,res){
-   // return res.end('<h1>User Profile</h1>');
-   
-   return res.render('users',{
-       title: "User Profile"
-   });
+module.exports.profile = function(req, res) {
+    // return res.end('<h1>User Profile</h1>');
+
+    return res.render('user_profile', {
+        title: "User Profile"
+    });
+}
+
+//render the signup page
+
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "Codeial | Sign Up"
+    });
+}
+
+//render the sign in page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: "Codeial | Sign In"
+    });
 }
