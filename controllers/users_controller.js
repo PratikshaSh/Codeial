8721da804106f5c1f302df2096838c41ce35ea72
PratikshaSh <pratiksha8520@gmail.com>
2021-03-14 19:48:29 +0530

    add user profile links + add update profile feature

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,19 +2,30 @@ const { user } = require('../config/mongoose');
 const User = require('../models/user');
 
 module.exports.profile = function(req, res) {
-    // return res.end('<h1>User Profile</h1>');
-
-    return res.render('user_profile', {
-        title: "User Profile"
+    User.findById(req.params.id, function(err, user){
+        
+        return res.render('user_profile', {
+            title: "User Profile",
+            profile_user: user
     });
-}
+    })
 
+}
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res. redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+}
 //render the signup page
 
 module.exports.signUp = function(req, res) {
 
     if(req.isAuthenticated()){
-        return res.redirect('/users/profile');
+        return res.redirect('/users/profile/<%= u.id %>');
     }
     return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
@@ -24,7 +35,7 @@ module.exports.signUp = function(req, res) {
 //render the sign in page
 module.exports.signIn = function(req, res) {
     if(req.isAuthenticated()){
-        return res.redirect('/users/profile');
+        return res.redirect('/users/profile/<%= u.id %>');
     }
     return res.render('user_sign_in', {
         title: "Codeial | Sign In"
