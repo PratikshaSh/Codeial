05d2c9c6bad3dc889f4db510fc90c2887cfb6a24
PratikshaSh <pratiksha8520@gmail.com>
2021-03-17 03:05:57 +0530

    setup profile upload

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,26 +1,70 @@
 const { user } = require('../config/mongoose');
 const User = require('../models/user');
+const fs = require('fs');
+const path = require('path');
 
 //lets keep it same as before
 module.exports.profile = function(req, res) {
+
     User.findById(req.params.id, function(err, user){
-        
         return res.render('user_profile', {
             title: "User Profile",
             profile_user: user
     });
-    })
+    });
 
 }
-module.exports.update = function(req, res){
+module.exports.update = async function(req, res){
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         req.flash('success', 'Updated!');
+    //         return res. redirect('back');
+    //     });
+    // }else{
+    //     req.flash('error', err);
+    //     return res.status(401).send('Unauthorized');
+    // }
+    
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            return res. redirect('back');
-        });
+
+        try {
+
+            let user = await User.findById(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if(err){console.log('*****Multer Error:', err); return;}
+                console.log(req.file);
+                user.name = req.body.name;
+                user.email = req.body.email;
+
+                if(req.file){
+                    if(user.avatar){
+                        fs.unlinkSync(path.join(__dirname,'..', user.avatar));
+                    }
+                    
+                    //this is saving the path of the uploaded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                console.log('saved');
+                return res.redirect('back');
+            });
+            
+        } catch (err) {
+            req.flash('error',err);
+            return res.redirect('back');
+        }
+
+
+
     }else{
+        req.flash('error', err);
         return res.status(401).send('Unauthorized');
     }
+
+
 }
+
+
 //render the signup page
 
 module.exports.signUp = function(req, res) {
