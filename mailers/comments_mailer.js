c70f3c8130720a4b05e794eabb55c9a3ba138b88
PratikshaSh <pratiksha8520@gmail.com>
2021-05-17 01:44:50 +0530

    done with sending html templaith sending html template mails

---
+++ mailers/comments_mailer.js
@@ -0,0 +1,20 @@
+const nodeMailer = require('../config/nodemailer');
+
+exports.newComment = (comment) => {
+    // console.log('inside newComment mailer', comment);
+    let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+    nodeMailer.transporter.sendMail({
+        from: 'Codeial@gmail.com',
+        to: comment.user.email,
+        subject: "New Comment Published",
+        html: htmlString
+    }, (err, info) => {
+        if(err){
+            console.log('Error in sending mail', err);
+            return ;
+        }
+        console.log('Message Sent!', info);
+        return ;
+    });
+}
